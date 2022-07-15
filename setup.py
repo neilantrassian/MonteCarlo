906a48814860759019e2b27453bcbb3be9b367ab
Neil Antrassian <neilantrassian@Neils-MacBook-Pro.local>
2022-07-15 10:14:06 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='MonteCarloProject',
+      version='0.1',
+      description='Final Project Package',
+      url='https://github.com/neilantrassian/MonteCarlo',
+      author='Neil Antrassian',
+      author_email='mst8gd@virginia.edu',
+      license='MIT',
+      packages=['Project'])
